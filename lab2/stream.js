dbfc916003928953b141efc9473125d30eddcad9
gakalu <69823298+gakalu@users.noreply.github.com>
2021-06-25 21:16:13 -0500

    Delete stream.js

--- lab2/stream.js
+++
@@ -1,8 +0,0 @@
-
-require("fs").createReadStream('./input.txt.gz')
-
-    .pipe(require('zlib').createGunzip())
-
-    .pipe(require("fs").createWriteStream('./input.txt'));
-    
-     console.log("File Decompressed.");
